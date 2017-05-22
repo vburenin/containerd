52c8f7ba94e3ac7c0787361c942f387502629236
Volodymyr Burenin <vburenin@gmail.com>
2017-05-22 10:58:29 -0500

    More code

--- plapi/vicruntime/runtime.go
+++ plapi/vicruntime/runtime.go
@@ -2,7 +2,6 @@ package vicruntime
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -12,7 +11,6 @@ import (
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/api/services/shim"
 	"github.com/containerd/containerd/api/types/container"
-	"github.com/containerd/containerd/api/types/mount"
 	"github.com/containerd/containerd/log"
 	"github.com/containerd/containerd/plapi/client"
 	"github.com/containerd/containerd/plapi/client/containers"
@@ -37,7 +35,7 @@ type Runtime struct {
 	root string
 
 	events        chan *containerd.Event
-	client        *client.PortLayer
+	pl            *client.PortLayer
 	eventsContext context.Context
 	eventsCancel  func()
 }
@@ -48,75 +46,44 @@ func New(ic *plugin.InitContext) (interface{}, error) {
 		return nil, err
 	}
 	c, cancel := context.WithCancel(ic.Context)
-	rt := &Runtime{
+	r := &Runtime{
 		root:          path,
 		events:        make(chan *containerd.Event, 2048),
 		eventsContext: c,
 		eventsCancel:  cancel,
-		client:        PortLayerClient(),
+		pl:            PortLayerClient(),
 	}
+	ic.Monitor.Events(r.events)
 
-	return rt, nil
+	return r, nil
 }
 
-func (r *Runtime) Create(ctx context.Context, id string, opts containerd.CreateOpts) (containerd.Container, error) {
+func (r *Runtime) Create(ctx context.Context, id string, opts plugin.CreateOpts) (plugin.Container, error) {
 	logrus.Debugf("Starting runtime for %s. Options: %q", id, opts)
 	path, err := r.newBundle(id, opts.Spec)
 	if err != nil {
 		return nil, err
 	}
 
-	shimSock := filepath.Join(path, "shim.sock")
-	s, err := NewPortLayerShim(r.root, shimSock, nil)
+	s, err := NewVicContainer(ctx, r.root, id, opts)
 	if err != nil {
 		os.RemoveAll(path)
 		return nil, err
 	}
-	if err := r.handleEvents(s); err != nil {
-		os.RemoveAll(path)
-		return nil, err
-	}
-	sopts := &shim.CreateRequest{
-		ID:       id,
-		Bundle:   path,
-		Runtime:  "runc",
-		Stdin:    opts.IO.Stdin,
-		Stdout:   opts.IO.Stdout,
-		Stderr:   opts.IO.Stderr,
-		Terminal: opts.IO.Terminal,
-	}
-	for _, m := range opts.Rootfs {
-		sopts.Rootfs = append(sopts.Rootfs, &mount.Mount{
-			Type:    m.Type,
-			Source:  m.Source,
-			Options: m.Options,
-		})
-	}
-	if _, err := s.Create(ctx, sopts); err != nil {
-		os.RemoveAll(path)
-		return nil, err
-	}
-	return &Container{
-		id:   id,
-		shim: s,
+
+	return s, err
+}
+
+func (r *Runtime) Delete(ctx context.Context, c plugin.Container) (*plugin.Exit, error) {
+	return &plugin.Exit{
+		Status:    0,
+		Timestamp: time.Now(),
 	}, nil
 }
 
-func (r *Runtime) Delete(ctx context.Context, c containerd.Container) (*containerd.Exit, error) {
-	lc, ok := c.(*Container)
-	if !ok {
-		return nil, fmt.Errorf("container cannot be cast as *linux.Container")
-	}
-	if _, err := lc.shim.Delete(ctx, &shim.DeleteRequest{}); err != nil {
-		return nil, err
-	}
-	lc.shim.Exit(ctx, &shim.ExitRequest{})
-	return nil, r.deleteBundle(lc.id)
-}
+func (r *Runtime) Containers(ctx context.Context) ([]plugin.Container, error) {
 
-func (r *Runtime) Containers(ctx context.Context) ([]containerd.Container, error) {
-
-	var o []containerd.Container
+	var o []plugin.Container
 
 	pl := PortLayerClient()
 	params := containers.NewGetContainerListParams().WithAll(swag.Bool(true))
@@ -127,28 +94,7 @@ func (r *Runtime) Containers(ctx context.Context) ([]containerd.Container, error
 	}
 
 	for _, c := range cl.Payload {
-		shimSock := filepath.Join(r.root, c.ContainerConfig.ContainerID, "shim.sock")
-		shimClient, err := NewPortLayerShim(r.root, shimSock, c)
-		if err != nil {
-			logrus.WithError(err).Warningf(
-				"Failed to initialize existing container %s",
-				c.ContainerConfig.ContainerID)
-		} else {
-
-			// Portlayer ID doesn't match containerd ID.
-			cnt := &Container{
-				id:   c.ContainerConfig.Annotations[AnnotationContainerdID],
-				shim: shimClient,
-			}
-			// If container is not created by containerd use Portlayer ID.
-			if cnt.id == "" {
-				cnt.id = c.ContainerConfig.ContainerID
-			}
-			logrus.Debugf("Restoring shim state for containerd ID %s (PL ID: %s)",
-				c.ContainerConfig.Annotations[AnnotationContainerdID],
-				c.ContainerConfig.ContainerID)
-			o = append(o, cnt)
-		}
+		o = append(o, RestoreVicContaner(ctx, r.root, c))
 	}
 
 	return o, nil
@@ -156,15 +102,6 @@ func (r *Runtime) Containers(ctx context.Context) ([]containerd.Container, error
 
 func (r *Runtime) Events(ctx context.Context) <-chan *containerd.Event {
 	return r.events
-}
-
-func (r *Runtime) handleEvents(s shim.ShimClient) error {
-	events, err := s.Events(r.eventsContext, &shim.EventsRequest{})
-	if err != nil {
-		return err
-	}
-	go r.forward(events)
-	return nil
 }
 
 func (r *Runtime) forward(events shim.Shim_EventsClient) {
